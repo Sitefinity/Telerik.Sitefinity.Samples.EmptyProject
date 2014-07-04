d4f40cb44e560d45e2fdb1a04ec67e0bebdd7116
nader-dab <nader.dabour@abv.bg>
2014-07-04 18:19:30 +0300

    Fixed StyleCop violations

--- SitefinityWebApp/Global.asax.cs
+++ SitefinityWebApp/Global.asax.cs
@@ -5,53 +5,44 @@ using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
 using Telerik.Sitefinity.Samples.Common;
+using Telerik.Sitefinity.Services;
 
 namespace SitefinityWebApp
 {
     public class Global : System.Web.HttpApplication
     {
-
         protected void Application_Start(object sender, EventArgs e)
         {
-            Telerik.Sitefinity.Abstractions.Bootstrapper.Initialized += new EventHandler<Telerik.Sitefinity.Data.ExecutedEventArgs>(Bootstrapper_Initialized);
+            SystemManager.ApplicationStart += SystemManager_ApplicationStart;
         }
 
-        protected void Bootstrapper_Initialized(object sender, Telerik.Sitefinity.Data.ExecutedEventArgs args)
+        private void SystemManager_ApplicationStart(object sender, EventArgs e)
         {
-            if (args.CommandName == "Bootstrapped")
-            {
-                SampleUtilities.CreateUsersAndRoles();                
-            }
+            SampleUtilities.CreateUsersAndRoles();     
         }
 
         protected void Session_Start(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_Error(object sender, EventArgs e)
         {
-
         }
 
         protected void Session_End(object sender, EventArgs e)
         {
-
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-
         }
     }
 }
