c8578d988b0ba0bcba5c44f31b2ec14c637ab0c2
Nader Dabour <nader.a.dabour@gmail.com>
2015-07-17 20:26:01 +0200

    Squashed 'Telerik.Sitefinity.Samples.Common/' changes from eb64565..6f03eb1
    6f03eb1 Merge commit '7bbf4b0d4a47cae7de0383206bb4a7375b5d177b' into HEAD
    7bbf4b0 added Microsoft.IdentityModel package that won't be found if you don't have Windows Identity Foundation
    3724f44 Merge commit 'a5bee2794c1cade1dfed9e64875a32af798ebcdd' into HEAD
    a5bee27 Upgraded project to 8.1.5800.0
    
    git-subtree-dir: Telerik.Sitefinity.Samples.Common
    git-subtree-split: 6f03eb13d5303435da5d0ae03fd489fea45c9c1f

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Telerik.Sitefinity.Samples.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Telerik AD")]
 [assembly: AssemblyProduct("Telerik.Sitefinity.Samples.Common")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Telerik AD 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
